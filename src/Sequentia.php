aa2665349da84a16519bae477eec5eb464bbce3c
Julian Hundeloh <julian@hundeloh-consulting.ch>
2023-02-09 16:58:14 +0000

    feat: add grant method

--- src/Sequentia.php
+++ src/Sequentia.php
@@ -2,9 +2,23 @@
 
 namespace Jaulz\Sequentia;
 
+use Illuminate\Support\Facades\DB;
+
 class Sequentia
 {
   public function getSchema() {
     return 'sequentia';
+  }
+
+  public function grant(string $role)
+  {
+    collect([
+      'GRANT USAGE ON SCHEMA %1\$s TO %2\$s',
+      'GRANT SELECT ON TABLE %1\$s.definitions TO %2\$s'
+    ])->each(fn (string $statement) => DB::statement(sprintf($statement, Sequentia::getSchema(), $role)));
+  }
+
+  public function ungrant(string $role)
+  {
   }
 }
